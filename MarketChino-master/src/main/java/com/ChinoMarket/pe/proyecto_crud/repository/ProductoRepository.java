15f4982c63bf32f605031aabf853917873e1b1ec
AndyBoyer07 <dazaboyer@gmail.com>
2025-04-19 17:11:24 -0500

    tercer commit servicios

--- MarketChino-master/src/main/java/com/ChinoMarket/pe/proyecto_crud/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ChinoMarket.pe.proyecto_crud.repository;
-
-import com.ChinoMarket.pe.proyecto_crud.entities.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    Producto findByNombre(String nombre);
-}
